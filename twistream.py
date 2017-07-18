dd85ae991deb93d18566a19fea7c7f4c55f6e191
Bhushan Deshmukh <bhushan.deshmukh2012@gmail.com>
2017-07-18 00:28:31 -0700

    Update twistream.py

--- twistream.py
+++ twistream.py
@@ -3,14 +3,11 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
-#Variables that contains the user credentials to access Twitter API 
 access_token = ""
 access_token_secret = ""
 consumer_key = ""
 consumer_secret = ""
 
-
-#This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
 
     def on_data(self, data):
@@ -23,11 +20,10 @@ class StdOutListener(StreamListener):
 
 if __name__ == '__main__':
 
-    #This handles Twitter authetification and the connection to Twitter Streaming API
     l = StdOutListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     stream = Stream(auth, l)
 
-    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+  
     stream.filter(track=['reykjavik'])
